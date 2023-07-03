c2d0d15c1d066e100d35ae6e9964bcd3fbb0f2b3
hoangvantuan123 <rammatuan@gmail.com>
2023-07-03 11:17:49 +0700

    update code

--- chat_python_reactjs/realtime_chat/realtime_chat/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from chat import views
-
-urlpatterns = [
-    path('api/messages/', views.message_list, name='message_list'),
-    path('create-group/', views.create_group),
-    path('add-member/<int:group_id>/', views.add_member),
-    path('send-message/<int:group_id>/', views.send_message),
-]
